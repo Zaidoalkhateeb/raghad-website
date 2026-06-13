c8420fb1a8172f8ec9b601ec93e92623c67bb52c
Zaid Alkhateeb <zaidomar2002@hotmail.com>
2026-06-13 16:43:37 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { BASE_MEMORIES, ICONS, REASONS, START_DATE } from './data.js';
 
 const PLACEHOLDER_SVG = (
